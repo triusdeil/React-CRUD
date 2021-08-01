aee30235ebbb073784c33e7917f90d6b02aeba00
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2021-08-01 19:45:31 -0400

    crud sencillo

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,115 @@
 import React, { useState } from "react";
 import shortid from "shortid";
+export default function App() {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(null);
 
-function App() {
-  const [task, setTask] = useState();
-  const [tasks, setTasks] = useState([]);
-
-  const AgregarTask = (e) => {
+  const agregarTarea = (e) => {
     e.preventDefault();
-    if (!task.trim()) {
+    if (!tarea.trim()) {
       console.log("elemento vacio");
+      setError("Ingrese algo por favor");
       return;
     }
-    console.log(task);
-    setTasks([
-      ...task,
-      {
-        id: shortid.generate(),
-        nameTask: task,
-      },
-    ]);
-    setTask("");
+    console.log(tarea);
+    setTareas([...tareas, { id: shortid.generate(), nombreTarea: tarea }]);
+    console.log(setTarea);
+    setTarea("");
+    setError(null);
+  };
+
+  const editarTarea = (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("elemento vacio");
+      setError("Ingrese algo por favor");
+      return;
+    }
+    const arrayEditado = tareas.map((item) =>
+      item.id === id ? { id: id, nombreTarea: tarea } : item
+    );
+    setTareas(arrayEditado);
+    setModoEdicion(false);
+    setTarea("");
+    setId("");
+    setError(null);
+  };
+
+  const eliminarTarea = (id) => {
+    const arrayFiltrado = tareas.filter((item) => item.id !== id);
+    setTareas(arrayFiltrado);
+  };
+
+  const editar = (item) => {
+    console.log(item);
+    setModoEdicion(true);
+    setTarea(item.nombreTarea);
+    setId(item.id);
   };
 
   return (
     <div className="container mt-5">
-      <div className="text-center">CRUD Simple</div>
+      <div className="text-center">Crud Simple</div>
       <hr />
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
-              <button className="btn btn-danger btn-sm float-end mx-2">
-                Eliminar
-              </button>
-              <button className="btn btn-warning btn-sm float-end">
-                Editar
-              </button>
-            </li>
+            {tareas.length === 0 ? (
+              <li className="list-group-item">No hay Tareas</li>
+            ) : (
+              tareas.map((item) => (
+                <li className="list-group-item" key={item.id}>
+                  <span className="lead">{item.nombreTarea}</span>
+                  <button
+                    className="btn btn-danger btn-sm float-end mx-2"
+                    onClick={() => eliminarTarea(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="btn btn-warning btn-sm float-end"
+                    onClick={() => editar(item)}
+                  >
+                    Editar
+                  </button>
+                </li>
+              ))
+            )}
           </ul>
         </div>
         <div className="col-4">
-          <h4 className="text-center">Formulario</h4>
-          <form onSubmit={AgregarTask}>
+          <h4 className="text-center">
+            {modoEdicion ? "Editar Tarea" : "Agregar Tarea"}
+          </h4>
+          <form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
+            {error ? <span className="text-danger">{error}</span> : null}
             <input
-              className="form-control mb-2"
-              placeholder="Ingrese Tarea"
               type="text"
-              onChange={(e) => setTask(e.target.value)}
-              value={task}
+              className="form-control mb-2"
+              placeholder="ingrese tarea"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
             />
-            <div className="d-grid gap-2">
-              <button className="btn btn-dark" type="submit">
-                Agregar
-              </button>
-            </div>
+            {modoEdicion ? (
+              <div className="d-grid gap-2">
+                <button className="btn btn-warning btn-block" type="submit">
+                  Editar
+                </button>
+              </div>
+            ) : (
+              <div className="d-grid gap-2">
+                <button className="btn btn-dark btn-block" type="submit">
+                  Agregar
+                </button>
+              </div>
+            )}
           </form>
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
